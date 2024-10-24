5170f6d85850b9e67af4538bb0715bd636409265
Visal-a-fernando <104492878+Visal-a-fernando@users.noreply.github.com>
2024-10-24 18:24:23 +0530

    Update home page title to include student ID

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.use('/index', indexRouter);
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 const port = process.env.PORT || 8080;
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'My Student ID is 20908566. Welcome to Express' });
+  //res.render('index', { title: 'My Student ID is 20908566. Welcome to Express' });
+  res.send('My Student ID is IT21163272. Welcome to Express');
 });
 
 export default router;
